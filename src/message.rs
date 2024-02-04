9d5d7aee1055aee8bc55f7d6711179a614b02b8e
sleepy ramen <rllola80@gmail.com>
2024-02-04 11:28:32 +0100

    feat: sepolia (#24)

--- src/message.rs
+++ src/message.rs
@@ -87,6 +87,43 @@ pub fn parse_transaction(payload: Vec<u8>) -> Transaction {
                     raw: eip_tx,
                 };
             }
+            3 => {
+                let t = rlp::Rlp::new(&eip_tx[1..]);
+                assert!(t.is_list());
+
+                // let chain_id: u32 = t.at(0).unwrap().as_val().unwrap();
+                let nonce: u32 = t.at(1).unwrap().as_val().unwrap();
+                // let max_priority_fee_per_gas: u64 = t.at(2).unwrap().as_val().unwrap();
+                // let max_fee_per_gas: u64 = t.at(3).unwrap().as_val().unwrap();
+                let gas_limit: u64 = t.at(4).unwrap().as_val().unwrap();
+                let to: Vec<u8> = t.at(5).unwrap().as_val().unwrap();
+                let value: Vec<u8> = t.at(6).unwrap().as_val().unwrap();
+                let data: Vec<u8> = t.at(7).unwrap().as_val().unwrap();
+                // let access_list: Vec<u8> = t.at(8).unwrap().as_val().unwrap();
+                // let max_fee_per_blob_gas: Vec<u8> = t.at(9).unwrap().as_val().unwrap();
+                // let blob_versioned_hashes: Vec<u8> = t.at(10).unwrap().as_val().unwrap();
+                let v: u32 = t.at(11).unwrap().as_val().unwrap();
+                let r: Vec<u8> = t.at(12).unwrap().as_val().unwrap();
+                let s: Vec<u8> = t.at(13).unwrap().as_val().unwrap();
+
+                let mut hasher = Keccak256::new();
+                hasher.update(&eip_tx);
+
+                return Transaction {
+                    txid: hasher.finalize().to_vec(),
+                    nonce,
+                    // we don't have gas_price but max_fee_per_gas and max_priority_fee_per_gas instead...
+                    gas_price: 0,
+                    gas_limit,
+                    to,
+                    value,
+                    data,
+                    v,
+                    r,
+                    s,
+                    raw: eip_tx,
+                };
+            }
             _ => {
                 dbg!(hex::encode(&payload));
                 todo!("others type not supported yet");
