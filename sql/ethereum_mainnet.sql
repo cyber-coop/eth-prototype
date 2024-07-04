c373ae22658fbcb2cec3eb2637517efcc1486f04
MehdiNdj <77308225+MehdiNdj@users.noreply.github.com>
2024-07-04 17:24:23 +0200

    feat: adding SQL index queries and logged tables (#43)
    Adding SQL index queries in SQL files and putting back tables creation
    as LOGGED because of multiple database losses during tests

--- sql/ethereum_mainnet.sql
+++ sql/ethereum_mainnet.sql
@@ -1,15 +1,21 @@
 -- Ethereum Mainnet
 
---- Quick Analyze
+--- Quick Analysis
 
 ANALYZE ethereum_mainnet.blocks;
 ANALYZE ethereum_mainnet.transactions;
+ANALYZE ethereum_mainnet.ommers;
 
---- Create Primary key and Foreign key
+--- Create Primary and Foreign keys
 ALTER TABLE ethereum_mainnet.blocks ADD CONSTRAINT hash_pk PRIMARY KEY (hash);
+ALTER TABLE ethereum_mainnet.blocks ADD CONSTRAINT number_fk FOREIGN KEY (number);
 ALTER TABLE ethereum_mainnet.transactions ADD CONSTRAINT txid_pk PRIMARY KEY (txid);
-ALTER TABLE ethereum_mainnet.transactions ADD CONSTRAINT block_fk FOREIGN KEY(block) REFERENCES ethereum_mainnet.blocks(hash);
+ALTER TABLE ethereum_mainnet.transactions ADD CONSTRAINT block_fk FOREIGN KEY (block) REFERENCES ethereum_mainnet.blocks (hash);
+ALTER TABLE ethereum_mainnet.ommers ADD CONSTRAINT phash_pk PRIMARY KEY (hash);
+ALTER TABLE ethereum_mainnet.ommers ADD CONSTRAINT ommersblock_fk FOREIGN KEY (canonical_hash) REFERENCES ethereum_mainnet.blocks (hash);
 
 --- Create Index
----- We use B-Tree indexing because later we want to do more insert of new transactions and it is supposedly faster 
-CREATE INDEX index_toaddress ON ethereum_mainnet.transactions using btree (toaddress);
+---- We use B-Tree indexing because we want to do more inserts of new transactions later and it is supposedly faster 
+CREATE INDEX i_blocknumber ON ethereum_mainnet.blocks using btree (number);
+CREATE INDEX i_txblock ON ethereum_mainnet.transactions using btree (block);
+CREATE INDEX i_ommerphash ON ethereum_mainnet.ommers using btree (canonical_hash);
--- sql/ethereum_ropsten.sql
+++ sql/ethereum_ropsten.sql
@@ -1,19 +1,25 @@
 -- Ethereum Ropsten
 
---- Quick Analyze
+--- Quick Analysis
 
 ANALYZE ethereum_ropsten.blocks;
 ANALYZE ethereum_ropsten.transactions;
+ANALYZE ethereum_ropsten.ommers;
 
 -- Ropsten have duplicates that we need to remove...
 CREATE TABLE ethereum_ropsten.tx_dup AS SELECT s.txid, s.block FROM ethereum_ropsten.transactions s JOIN (SELECT txid, count(*) FROM ethereum_ropsten.transactions GROUP BY txid HAVING COUNT(*) > 1) d ON s.txid = d.txid ORDER BY s.txid;
 DELETE FROM ethereum_ropsten.transactions t USING (SELECT DISTINCT ON (txid) * FROM ethereum_ropsten.tx_dup) dup WHERE t.txid = dup.txid AND t.block = dup.block;
 
---- Create Primary key and Foreign key
+--- Create Primary and Foreign keys
 ALTER TABLE ethereum_ropsten.blocks ADD CONSTRAINT hash_pk PRIMARY KEY (hash);
+ALTER TABLE ethereum_ropsten.blocks ADD CONSTRAINT number_fk FOREIGN KEY (number);
 ALTER TABLE ethereum_ropsten.transactions ADD CONSTRAINT txid_pk PRIMARY KEY (txid);
-ALTER TABLE ethereum_ropsten.transactions ADD CONSTRAINT block_fk FOREIGN KEY(block) REFERENCES ethereum_ropsten.blocks(hash);
+ALTER TABLE ethereum_ropsten.transactions ADD CONSTRAINT block_fk FOREIGN KEY (block) REFERENCES ethereum_ropsten.blocks (hash);
+ALTER TABLE ethereum_ropsten.ommers ADD CONSTRAINT phash_pk PRIMARY KEY (hash);
+ALTER TABLE ethereum_ropsten.ommers ADD CONSTRAINT ommersblock_fk FOREIGN KEY (canonical_hash) REFERENCES ethereum_ropsten.blocks (hash);
 
 --- Create Index
----- We use B-Tree indexing because later we want to do more insert of new transactions and it is supposedly faster 
-CREATE INDEX index_toaddress ON ethereum_ropsten.transactions using btree (toaddress);
+---- We use B-Tree indexing because we want to do more inserts of new transactions later and it is supposedly faster 
+CREATE INDEX i_blocknumber ON ethereum_ropsten.blocks using btree (number);
+CREATE INDEX i_txblock ON ethereum_ropsten.transactions using btree (block);
+CREATE INDEX i_ommerphash ON ethereum_ropsten.ommers using btree (canonical_hash);
