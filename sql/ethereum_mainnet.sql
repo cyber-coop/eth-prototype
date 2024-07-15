7188c5c9d104abb9bdff8642897688c7b2cd10e9
MehdiNdj <mehdi.nedjar60@gmail.com>
2024-07-15 15:20:14 +0200

    fix: correcting pk and index creation queries

--- sql/ethereum_mainnet.sql
+++ sql/ethereum_mainnet.sql
@@ -9,7 +9,6 @@ ANALYZE ethereum_mainnet.ommers;
 --- Create Primary and Foreign keys
 ALTER TABLE ethereum_mainnet.blocks ADD CONSTRAINT hash_pk PRIMARY KEY (hash);
 ALTER TABLE ethereum_mainnet.blocks ADD CONSTRAINT number_fk FOREIGN KEY (number);
-ALTER TABLE ethereum_mainnet.transactions ADD CONSTRAINT txid_pk PRIMARY KEY (txid);
 ALTER TABLE ethereum_mainnet.transactions ADD CONSTRAINT block_fk FOREIGN KEY (block) REFERENCES ethereum_mainnet.blocks (hash);
 ALTER TABLE ethereum_mainnet.ommers ADD CONSTRAINT phash_pk PRIMARY KEY (hash);
 ALTER TABLE ethereum_mainnet.ommers ADD CONSTRAINT ommersblock_fk FOREIGN KEY (canonical_hash) REFERENCES ethereum_mainnet.blocks (hash);
@@ -17,5 +16,5 @@ ALTER TABLE ethereum_mainnet.ommers ADD CONSTRAINT ommersblock_fk FOREIGN KEY (c
 --- Create Index
 ---- We use B-Tree indexing because we want to do more inserts of new transactions later and it is supposedly faster 
 CREATE INDEX i_blocknumber ON ethereum_mainnet.blocks using btree (number);
-CREATE INDEX i_txblock ON ethereum_mainnet.transactions using btree (block);
+CREATE INDEX i_txid ON ethereum_mainnet.transactions using btree (txid);
 CREATE INDEX i_ommerphash ON ethereum_mainnet.ommers using btree (canonical_hash);
