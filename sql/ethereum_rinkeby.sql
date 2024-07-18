eb62d6c03870b7f75b42579a876c02dd0095a9e6
MehdiNdj <77308225+MehdiNdj@users.noreply.github.com>
2024-07-18 19:39:14 +0200

    Fix: correcting pk and index creation queries (#48)
    Removing wrong primary key creation (txid not unique) and correcting
    index creation for txids (instead of blocks)

--- sql/ethereum_rinkeby.sql
+++ sql/ethereum_rinkeby.sql
@@ -12,8 +12,6 @@ DELETE FROM ethereum_rinkeby.transactions t USING (SELECT DISTINCT ON (txid) * F
 
 --- Create Primary and Foreign keys
 ALTER TABLE ethereum_rinkeby.blocks ADD CONSTRAINT hash_pk PRIMARY KEY (hash);
-ALTER TABLE ethereum_rinkeby.blocks ADD CONSTRAINT number_fk FOREIGN KEY (number);
-ALTER TABLE ethereum_rinkeby.transactions ADD CONSTRAINT txid_pk PRIMARY KEY (txid);
 ALTER TABLE ethereum_rinkeby.transactions ADD CONSTRAINT block_fk FOREIGN KEY (block) REFERENCES ethereum_rinkeby.blocks (hash);
 ALTER TABLE ethereum_rinkeby.ommers ADD CONSTRAINT phash_pk PRIMARY KEY (hash);
 ALTER TABLE ethereum_rinkeby.ommers ADD CONSTRAINT ommersblock_fk FOREIGN KEY (canonical_hash) REFERENCES ethereum_rinkeby.blocks (hash);
@@ -21,5 +19,5 @@ ALTER TABLE ethereum_rinkeby.ommers ADD CONSTRAINT ommersblock_fk FOREIGN KEY (c
 --- Create Index
 ---- We use B-Tree indexing because we want to do more inserts of new transactions later and it is supposedly faster 
 CREATE INDEX i_blocknumber ON ethereum_rinkeby.blocks using btree (number);
-CREATE INDEX i_txblock ON ethereum_rinkeby.transactions using btree (block);
+CREATE INDEX i_txid ON ethereum_rinkeby.transactions using btree (txid);
 CREATE INDEX i_ommerphash ON ethereum_rinkeby.ommers using btree (canonical_hash);
