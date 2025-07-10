28ffe98047c90173426f9f50eb5d007690201af0
sleepy ramen <rllola80@gmail.com>
2025-07-11 00:23:57 +0200

    fix: goerli forkid networks (#85)

--- src/main.rs
+++ src/main.rs
@@ -332,35 +332,37 @@ fn run(
          *
          ******************/
 
-        info!("Get safe block hash");
-        let get_blocks_headers =
-            eth::create_get_block_headers_message(&current_hash, 2, 1024, true);
-        utils::send_message(
-            get_blocks_headers,
-            &mut stream,
-            &mut egress_mac,
-            &mut egress_aes,
-        )?;
+        // info!("Get safe block hash");
+        // let get_blocks_headers =
+        //     eth::create_get_block_headers_message(&current_hash, 2, 1024, true);
+        // utils::send_message(
+        //     get_blocks_headers,
+        //     &mut stream,
+        //     &mut egress_mac,
+        //     &mut egress_aes,
+        // )?;
 
-        let mut uncrypted_body: Vec<u8>;
-        let mut code;
-        loop {
-            uncrypted_body = utils::read_message(&mut stream, &mut ingress_mac, &mut ingress_aes)?;
+        // let mut uncrypted_body: Vec<u8>;
+        // let mut code;
+        // loop {
+        //     uncrypted_body = utils::read_message(&mut stream, &mut ingress_mac, &mut ingress_aes)?;
 
-            if uncrypted_body[0] > 16 {
-                code = uncrypted_body[0] - 16;
-                if code == 4 {
-                    break;
-                }
-            }
-        }
+        //     if uncrypted_body[0] > 16 {
+        //         code = uncrypted_body[0] - 16;
+        //         if code == 4 {
+        //             break;
+        //         }
+        //     }
+        // }
 
-        assert_eq!(code, 4);
+        // assert_eq!(code, 4);
 
-        let block_headers = eth::parse_block_headers(uncrypted_body[1..].to_vec());
+        // let block_headers = eth::parse_block_headers(uncrypted_body[1..].to_vec());
 
-        // update block hash
-        *current_hash = block_headers.last().unwrap().parent_hash.to_vec();
+        // dbg!(&block_headers);
+
+        // // update block hash
+        // *current_hash = block_headers.last().unwrap().parent_hash.to_vec();
     }
 
     /****************************
