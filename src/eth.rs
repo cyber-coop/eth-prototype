e5aa9c9b83243ab4b74f09b8c0cd6b872b88f885
rllola <rllola80@gmail.com>
2025-07-11 00:23:34 +0200

    fix fork id for rinkeby

--- src/eth.rs
+++ src/eth.rs
@@ -51,7 +51,7 @@ pub fn parse_status_message(payload: Vec<u8>) -> Vec<u8> {
 
     let _version: u16 = r.at(0).unwrap().as_val().unwrap();
     let _network_id: u64 = r.at(1).unwrap().as_val().unwrap();
-    // let _td: u16 = r.at(2).unwrap().as_val().unwrap();
+    // let _td: u64 = r.at(2).unwrap().as_val().unwrap();
     let blockhash: Vec<u8> = r.at(3).unwrap().as_val().unwrap();
     let _genesis: Vec<u8> = r.at(4).unwrap().as_val().unwrap();
 
